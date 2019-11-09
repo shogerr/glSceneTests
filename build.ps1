e7bfa2d5a38ae022d5f5d1411906eef8e2f602f2
shogerr <shogerr@oregonstate.edu>
2019-11-08 20:46:11 -0800

    adjust folders

--- build.ps1
+++ build.ps1
@@ -1,3 +1,12 @@
+Param(
+    [Switch]$Clean
+)
+
+if ($Clean -eq $True) {
+    git clean -xdf -e glad -e .vs -e *.swp -e CMakeSettings.json
+    exit
+}
+
 cmake -B build -S . -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_TOOLCHAIN_FILE="$env:VCPKG\scripts\buildsystems\vcpkg.cmake"
 cmake --build build
 cmake --install build --prefix .
